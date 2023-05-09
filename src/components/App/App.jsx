a0e0e24cc19b9f21eac638b5b388c359d69376f0
annabusaga <anna.busaga@gmail.com>
2023-05-09 20:14:41 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,25 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(
+      localStorage.getItem('contacts') || [
+        {
+          id: 'id-1',
+          name: 'Rosie Simpson',
+          number: '459-12-56',
+        },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+    this.setState({ contacts });
+  }
 
   formSubmitHandler = ({ name, number }) => {
     if (this.state.contacts.some(contact => contact.name === name)) {
@@ -44,7 +55,7 @@ export class App extends Component {
     this.setState({ filter: event.currentTarget.value });
   };
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(prevProp, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
